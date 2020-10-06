d02e855ec4697643f5423a1e8a797949914bd3f3
Thycker <nikki090298@hotmail.com>
2020-10-05 21:48:04 -0400

    avancé#3

---
+++ Avion/Avion_Jeux_Pratique/Assets/MoveEnnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnnemy : MonoBehaviour
+{
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveLeft();
+    }
+
+    private void moveLeft()
+    {
+        transform.Translate(-1*Time.deltaTime * 2, 0, 0);
+    }
+}
---
+++ Avion/Avion_Jeux_Pratique/Assets/spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public float spawnRate = 1;
+    private float timeLeftBeforeSpawn = 0;
+    private SpawnPoint[] spawnPoints;
+    public GameObject ennemiTrianglePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoints = FindObjectsOfType<SpawnPoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSpawn();
+    }
+
+    private void UpdateSpawn()
+    {
+        timeLeftBeforeSpawn -= Time.deltaTime;
+        if (timeLeftBeforeSpawn <= 0)
+        {
+            SpawnEnnemiTriangle();
+            timeLeftBeforeSpawn = spawnRate;
+        }
+    }
+
+    private void SpawnEnnemiTriangle()
+    {
+        int countSpawnPoints = spawnPoints.Length;
+        int randomSpawnPointIndex = Random.Range(0, countSpawnPoints);
+        SpawnPoint currentSpawnPoint = spawnPoints[randomSpawnPointIndex];
+        Instantiate(ennemiTrianglePrefab, currentSpawnPoint.transform.position, Quaternion.identity);
+    }
+}
